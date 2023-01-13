1c27423c318392b78a5347468250e276f80d571c
João Pedro Diniz <joaopedrodiniz44@gmail.com>
2023-01-13 10:35:16 -0300

    feat(app): add função de proucurar pokemon

--- app.js
+++ app.js
@@ -23,4 +23,20 @@ function createBox(element,x){
             <p class="pokemonName">${element.name}</p>
             <p class="pokemonType">${element.types.map(typeInfo => typeInfo.type.name).join(' | ')} </p>
         </li>`
- }
+}
+
+function search_pokemon() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByTagName('li');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="flex";
+            x[i].style.height="310px"             
+        }
+    }
+}
