658f808a5ff12be461a57f00789d0e10be2f05a1
João Pedro Diniz <joaopedrodiniz44@gmail.com>
2023-01-29 21:26:04 -0300

    feat(app): add async

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
 let fetchConfig = {
     method: "GET"
 }
-
+const body = document.querySelector('body')
 var div = document.querySelectorAll("ol")[0]
 var id = 0   
-for ( i = 1; i < 152; i++){
-    var urlApi = 'https://pokeapi.co/api/v2/pokemon/' + String(i)
-    fetch(urlApi,fetchConfig)
-            .then(resposta => resposta.json()).then((data) => {
-                // console.log(data)
-                let pokemons = data.results
-                createBox(data,id+=1);
-            })
-}
-   
+const allPokemons = 152 
+
+
+async function getPokemons(){ 
+    for ( i = 1; i < 9; i++){
+        var urlApi = 'https://pokeapi.co/api/v2/pokemon/' + String(i)
+        await fetch(urlApi,fetchConfig)
+                .then(resposta => resposta.json()).then((data) => {
+                    console.log(data)
+                    let pokemons = data.results
+                    createBox(data,id+=1);
+                })
+    }
+}   
+
+getPokemons();
+
 function createBox(element,x){
     div.innerHTML += 
-        `<li class="card_${element.types.map(typeInfo => typeInfo.type.name)[0]}" id="pokemons">
+        `<li class="card_${element.types.map(typeInfo => typeInfo.type.name)[0]}" id="pokemons" onclick ='viewPoke(${id})'>
             <div class="circle"></div>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${element.id}.gif" class="pokemonImage" alt="foto do pokemon ${element.name}" id="foto">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${element.id}.png" class="pokemonImage" alt="foto do pokemon ${element.name}" id="foto">
             <p class="pokemonId">#${id.toString().padStart(3,0)}</p>
             <p class="pokemonName">${element.name}</p>
             <p class="pokemonType">${element.types.map(typeInfo => typeInfo.type.name).join(' | ')} </p>
         </li>`
-}
+ }
 
 function search_pokemon() {
     let input = document.getElementById('searchbar').value
